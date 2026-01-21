26a6240ae7fd97d460123644ab8d2597323fdf6b
ezerfrlux <aaron.salinas318@gmail.com>
2026-01-21 16:19:51 -0600

    feat: start timer function using states

---
+++ src/hooks/useTimer.ts
@@ -0,0 +1,33 @@
+import { useRef, useState } from "react";
+
+export function useTimer() {
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
+
+  const intervalRef = useRef<number | null>(null);
+  const startTimeRef = useRef<number>(0);
+
+  const start = () => {
+    if (running) return;
+
+    startTimeRef.current = Date.now() - time;
+
+    intervalRef.current = window.setInterval(() => {
+      setTime(Date.now() - startTimeRef.current);
+    }, 10);
+
+    setRunning(true);
+  };
+
+  const stop = () => {};
+
+  const reset = () => {};
+
+  return {
+    time,
+    running,
+    start,
+    stop,
+    reset,
+  };
+}
